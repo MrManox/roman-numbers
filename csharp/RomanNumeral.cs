a70e9fbef0874c3b3a635fef51af20af5b9c0505
Marco Wagner <marco.wagner@oberasbach.net>
2020-01-14 12:20:37 +0100

    translated to c#

---
+++ csharp/RomanNumeral.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace csharp
+{
+    public class RomanNumeral
+    {
+        private static int[] numbers = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        private static String[] letters = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+        public String ConvertArabicToRomanNumeral (int arabic)
+        {
+            //check input
+            if (arabic < 1)
+                throw new ArgumentException ("Value of RomanNumeral must be positive.");
+            if (arabic > 3999)
+                throw new ArgumentException ("Value of RomanNumeral must be 3999 or less.");
+
+            //build numeral
+            String roman = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (arabic >= numbers[i])
+                {
+                    roman += letters[i];
+                    arabic -= numbers[i];
+                }
+            }
+            return roman;
+        }
+
+    }
+}
---
+++ csharp/RomanNumeralTest.cs
@@ -0,0 +1,107 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace csharp
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+
+        RomanNumeral rn;
+
+        [TestInitialize ()]
+        public void before ()
+        {
+            rn = new RomanNumeral ();
+        }
+
+        [TestMethod]
+        public void TestOn1 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (1);
+            String exp = "I";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn5 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (5);
+            String exp = "V";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn10 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (10);
+            String exp = "X";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn50 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (50);
+            String exp = "L";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn100 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (100);
+            String exp = "C";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn500 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (500);
+            String exp = "D";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn1000 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (1000);
+            String exp = "M";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn11 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (11);
+            String exp = "XI";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn9 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (9);
+            String exp = "IX";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn16 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (16);
+            String exp = "XVI";
+            Assert.AreEqual (exp, res);
+        }
+
+        [TestMethod]
+        public void TestOn1999 ()
+        {
+            String res = rn.ConvertArabicToRomanNumeral (1999);
+            String exp = "MCMXCIX";
+            Assert.AreEqual (exp, res);
+        }
+
+    }
+}
